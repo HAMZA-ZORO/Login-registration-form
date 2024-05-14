96e56d4782a4bef4a4e118d6bbee02f9b09fa840
Hamza Mahmoud <116195770+HAMZA-ZORO@users.noreply.github.com>
2024-05-14 20:17:04 +0300

    user.sql

--- user.sql
+++ user.sql
@@ -1,6 +1,5 @@
 create table enroll.user(
-user_id int AUTO_INCREMENT primary key,
 user_name varchar(20) not null,
-email varchar(50) not null,
+email varchar(50) primary key,
 pass varchar(255)
 );
